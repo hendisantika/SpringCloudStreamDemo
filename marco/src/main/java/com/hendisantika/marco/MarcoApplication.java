0e9349b6cbc8d4e11d52798d7dc38e0a7072efad
Hendi Santika <hendisantika@gmail.com>
2018-03-18 12:55:28 +0700

    #1 - Initial Commit - marco module

---
+++ marco/src/main/java/com/hendisantika/marco/MarcoApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.marco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcoApplication.class, args);
+	}
+}
---
+++ marco/src/test/java/com/hendisantika/marco/MarcoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.marco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarcoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
