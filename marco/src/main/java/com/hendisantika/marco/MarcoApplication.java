613fbb5cd1806d93b1dacc06204ed9e637769a8c
Hendi Santika <hendisantika@gmail.com>
2018-03-18 12:58:50 +0700

    #4 - Modify Main App - marco module

--- marco/src/main/java/com/hendisantika/marco/MarcoApplication.java
+++ marco/src/main/java/com/hendisantika/marco/MarcoApplication.java
@@ -2,11 +2,24 @@ package com.hendisantika.marco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.GenericMessage;
 
 @SpringBootApplication
+@EnableBinding(Source.class)
 public class MarcoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarcoApplication.class, args);
 	}
+
+    @Bean
+    @InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "1000", maxMessagesPerPoll = "1"))
+    public MessageSource<String> timerMessageSource() {
+        return () -> new GenericMessage<>("MARCO: " + System.currentTimeMillis());
+    }
 }
