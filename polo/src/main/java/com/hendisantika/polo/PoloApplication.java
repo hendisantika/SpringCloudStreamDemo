d9b3e1028c91ddb7c188b3b7f4cbd95fb8844f22
Hendi Santika <hendisantika@gmail.com>
2018-03-19 20:41:17 +0700

    #6 - Initial Commit - polo module

---
+++ polo/src/main/java/com/hendisantika/polo/PoloApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.polo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoloApplication.class, args);
+	}
+}
