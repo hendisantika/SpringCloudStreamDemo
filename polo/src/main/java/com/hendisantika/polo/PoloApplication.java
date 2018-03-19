d0fd8f6cd9334002f27e6121920db1e3d9c8d218
Hendi Santika <hendisantika@gmail.com>
2018-03-19 20:45:02 +0700

    #9 - Modify Main App - polo project

--- polo/src/main/java/com/hendisantika/polo/PoloApplication.java
+++ polo/src/main/java/com/hendisantika/polo/PoloApplication.java
@@ -1,12 +1,25 @@
 package com.hendisantika.polo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.integration.annotation.ServiceActivator;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class PoloApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(PoloApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoloApplication.class, args);
 	}
+
+    @ServiceActivator(inputChannel = Sink.INPUT)
+    public void loggerSink(Object payload) throws Exception {
+        logger.info("POLO: " + payload);
+        Thread.sleep(60000);
+    }
 }
